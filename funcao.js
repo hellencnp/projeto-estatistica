f77d1c6aa38d87228a6084f31164bd31ebfecaf4
hellencnp <hellen.nascii@hotmail.com>
2020-09-05 16:14:00 -0300

    primeira parte do front

---
+++ funcao.js
@@ -0,0 +1,9 @@
+function checka() {
+    document.getElementById("amostra").checked = true;
+    document.getElementById("populacao").checked = false;
+}
+function checkp() {
+    document.getElementById("populacao").checked = true;
+    document.getElementById("amostra").checked = false;
+
+}
